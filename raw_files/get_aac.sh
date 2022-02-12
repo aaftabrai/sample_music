4a127123c59d03ec9d1a5cf7240c649d4d0dc918
Aftab Rai <aftabra@amazon.com>
2022-02-11 17:33:08 -0800

    typo in playlist

--- raw_files/get_aac.sh
+++ raw_files/get_aac.sh
@@ -1,5 +1,5 @@
-ffmpeg -i https://yats-alex.s3.us-west-1.amazonaws.com/disco/9538100 -vn -c:a copy raw_files/9538100.aac
-ffmpeg -i https://yats-alex.s3.us-west-1.amazonaws.com/disco/9538101 -vn -c:a copy raw_files/9538101.aac
-ffmpeg -i https://yats-alex.s3.us-west-1.amazonaws.com/disco/9538102 -vn -c:a copy raw_files/9538102.aac
-ffmpeg -i https://yats-alex.s3.us-west-1.amazonaws.com/disco/9538103 -vn -c:a copy raw_files/9538103.aac
-ffmpeg -i https://yats-alex.s3.us-west-1.amazonaws.com/disco/9538104 -vn -c:a copy raw_files/9538104.aac
+ffmpeg -i https://yats-alex.s3.us-west-1.amazonaws.com/disco/9538100 -f mpegts -acodec copy 9538100.ts
+ffmpeg -i https://yats-alex.s3.us-west-1.amazonaws.com/disco/9538101 -f mpegts -acodec copy 9538101.ts
+ffmpeg -i https://yats-alex.s3.us-west-1.amazonaws.com/disco/9538102 -f mpegts -acodec copy 9538102.ts
+ffmpeg -i https://yats-alex.s3.us-west-1.amazonaws.com/disco/9538103 -f mpegts -acodec copy 9538103.ts
+ffmpeg -i https://yats-alex.s3.us-west-1.amazonaws.com/disco/9538104 -f mpegts -acodec copy 9538104.ts
