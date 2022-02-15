f66853484c9944451d863df7ae9eb4fb8c5b0d82
Aftab Rai <aftabra@amazon.com>
2022-02-15 01:18:50 -0800

    add aac playlist

---
+++ raw_files/aac/get_aac.sh
@@ -0,0 +1,6 @@
+ffmpeg -i https://yats-alex.s3.us-west-1.amazonaws.com/disco/pri-aac96-1631739381-5480-37156.ts -vn -c:a copy pri-aac96-1631739381-5480-37156.ts.aac
+ffmpeg -i https://yats-alex.s3.us-west-1.amazonaws.com/disco/pri-aac96-1631739386-5480-37157.ts -vn -c:a copy pri-aac96-1631739386-5480-37157.ts.aac
+ffmpeg -i https://yats-alex.s3.us-west-1.amazonaws.com/disco/pri-aac96-1631739392-5480-37158.ts -vn -c:a copy pri-aac96-1631739392-5480-37158.ts.aac
+ffmpeg -i https://yats-alex.s3.us-west-1.amazonaws.com/disco/pri-aac96-1631739397-5480-37159.ts -vn -c:a copy pri-aac96-1631739397-5480-37159.ts.aac
+ffmpeg -i https://yats-alex.s3.us-west-1.amazonaws.com/disco/pri-aac96-1631739403-5480-37160.ts -vn -c:a copy pri-aac96-1631739403-5480-37160.ts.aac
+ffmpeg -i https://yats-alex.s3.us-west-1.amazonaws.com/disco/pri-aac96-1631739408-2507-37161.ts -vn -c:a copy pri-aac96-1631739408-2507-37161.ts.aac
