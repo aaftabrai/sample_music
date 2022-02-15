9d6b4f8411923c8af7f44c143777c8d29545cc4d
Aftab Rai <aftabra@amazon.com>
2022-02-14 16:31:43 -0800

    14 feb converted track

---
+++ 14feb/raw/content_con/convert_content.sh
@@ -0,0 +1,7 @@
+ffmpeg -i https://github.com/aaftabrai/sample_music/blob/main/14feb/raw/original/pri-aac96-1644826470-5480-50444.ts?raw=true -f mpegts -acodec copy pri-aac96-1644826470-5480-50444.ts?raw=true
+ffmpeg -i https://github.com/aaftabrai/sample_music/blob/main/14feb/raw/original/pri-aac96-1644826476-5480-50445.ts?raw=true -f mpegts -acodec copy pri-aac96-1644826476-5480-50445.ts?raw=true
+ffmpeg -i https://github.com/aaftabrai/sample_music/blob/main/14feb/raw/original/pri-aac96-1644826481-5480-50446.ts?raw=true -f mpegts -acodec copy pri-aac96-1644826481-5480-50446.ts?raw=true
+ffmpeg -i https://github.com/aaftabrai/sample_music/blob/main/14feb/raw/original/pri-aac96-1644826487-5480-50447.ts?raw=true -f mpegts -acodec copy pri-aac96-1644826487-5480-50447.ts?raw=true
+ffmpeg -i https://github.com/aaftabrai/sample_music/blob/main/14feb/raw/original/pri-aac96-1644826492-5480-50448.ts?raw=true -f mpegts -acodec copy pri-aac96-1644826492-5480-50448.ts?raw=true
+ffmpeg -i https://github.com/aaftabrai/sample_music/blob/main/14feb/raw/original/pri-aac96-1644826498-5480-50449.ts?raw=true -f mpegts -acodec copy pri-aac96-1644826498-5480-50449.ts?raw=true
+ffmpeg -i https://github.com/aaftabrai/sample_music/blob/main/14feb/raw/original/pri-aac96-1644826503-2763-50450.ts?raw=true -f mpegts -acodec copy pri-aac96-1644826503-2763-50450.ts?raw=true
